583f4fb17767fc79b47f9a1bfab2328cb8fa224a
James Stone <james.stone@ovo.com>
2022-04-20 09:25:44 +0100

    Add ovo.com domain

--- app/AppComponents.scala
+++ app/AppComponents.scala
@@ -41,7 +41,7 @@ class AppComponents(context: Context, config: Config)
       clientId = config.google.clientId,
       clientSecret = config.google.clientSecret.value,
       redirectUrl = config.google.redirectUrl,
-      domains = List("ovoenergy.com", "kaluza.com"),
+      domains = List("ovo.com", "ovoenergy.com", "kaluza.com"),
       antiForgeryChecker = AntiForgeryChecker(
         InitialSecret(httpConfiguration.secret.secret),
         AntiForgeryChecker.signatureAlgorithmFromPlay(httpConfiguration)
